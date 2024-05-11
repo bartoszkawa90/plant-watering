4440f1d668e6e4945323df456b3ff7142a3c46ec
Bartosz_Kawa <bartekkawa2@gmail.com>
2024-05-12 00:44:27 +0200

    webui works for sure

--- main/webui.c
+++ main/webui.c
@@ -12,6 +12,8 @@
 #include "nvs.h"
 #include "wifi.c"
 
+#include "pages.c"
+
 // Replace with your Wi-Fi credentials
 #define WIFI_SSID "Galaxy M213EBB"
 #define WIFI_PASS "wwmz5043"
@@ -20,31 +22,6 @@
 static const char *TAG_webui = "WEBUI";
 
 static bool led_state = false;
-
-// update led
-const char *html_page = "<html><body>"
-                        "<h1>ESP32 LED Control</h1>"
-                        "<p id=\"ledState\">LED is OFF</p>" // Default text
-                        "<button onclick=\"toggleLED(); updateLEDState();\">Toggle LED</button>"
-                        "<script>"
-                        "function toggleLED() {"
-                        "  var xhr = new XMLHttpRequest();"
-                        "  xhr.open('GET', '/toggle', true);"
-                        "  xhr.send();"
-                        "}"
-                        "function updateLEDState() {"
-                        "  var xhr = new XMLHttpRequest();"
-                        "  xhr.onreadystatechange = function() {"
-                        "    if (xhr.readyState == 4 && xhr.status == 200) {"
-                        "      document.getElementById('ledState').innerHTML = xhr.responseText;"
-                        "    }"
-                        "  };"
-                        "  xhr.open('GET', '/led-state', true);"
-                        "  xhr.send();"
-                        "}"
-                        "</script>"
-                        "</body></html>";
-
 
 /* An HTTP GET handler for the root page */
 esp_err_t get_handler(httpd_req_t *req) {
@@ -57,28 +34,12 @@ esp_err_t toggle_handler(httpd_req_t *req) {
     // static bool led_state = false;
     led_state = !led_state;
     gpio_set_level(LED_PIN, led_state);
-    httpd_resp_send(req, NULL, 0); // Send an empty response to acknowledge
-    return ESP_OK;
-}
-
-esp_err_t get_led_state_handler(httpd_req_t *req) {
-    // char* resp_str = (gpio_get_level(LED_PIN) == 1) ? "LED is ON" : "LED is OFF";
-    // printf("%s", " --- MAIN --- LED state");
-    // printf("%d", gpio_get_level(LED_PIN));
-    // char* resp_str;
-    // if (gpio_get_level(LED_PIN) == 1){
-    //     resp_str = "LED is ON";
-    // }
-    // else{
-    //     resp_str = "LED is OFF";
-    // }
-    // httpd_resp_send(req, resp_str, HTTPD_RESP_USE_STRLEN);
-    char* resp_str;
+        char* resp_str;
     if (led_state){
-        resp_str = "LED is ON";
+        resp_str = "ON";
     }
     else{
-        resp_str = "LED is OFF";
+        resp_str = "OFF";
     }
     httpd_resp_send(req, resp_str, HTTPD_RESP_USE_STRLEN);
     return ESP_OK;
@@ -99,13 +60,6 @@ httpd_uri_t uri_toggle = {
     .user_ctx  = NULL
 };
 
-httpd_uri_t uri_get_led_state = {
-    .uri       = "/led-state",
-    .method    = HTTP_GET,
-    .handler   = get_led_state_handler,
-    .user_ctx  = NULL
-};
-
 // start web server
 httpd_handle_t start_webserver(void) {
     httpd_config_t config = HTTPD_DEFAULT_CONFIG();
@@ -114,7 +68,6 @@ httpd_handle_t start_webserver(void) {
     if (httpd_start(&server, &config) == ESP_OK) {
         httpd_register_uri_handler(server, &uri_get);
         httpd_register_uri_handler(server, &uri_toggle);
-        httpd_register_uri_handler(server, &uri_get_led_state);
     }
     return server;
 }
