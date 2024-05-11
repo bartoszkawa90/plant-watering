e7e98fd62e95950363b712bbd00050cd480a058a
Bartosz_Kawa <bartekkawa2@gmail.com>
2024-05-11 11:28:03 +0200

    webui works

--- main/webui.c
+++ main/webui.c
@@ -17,20 +17,34 @@
 #define WIFI_PASS "wwmz5043"
 #define LED_PIN 2 // GPIO pin dla niebieskiego leda z prawej
 
-static const char *TAG = "example";
+static const char *TAG_webui = "WEBUI";
 
-/* HTML Page with Button */
+static bool led_state = false;
+
+// update led
 const char *html_page = "<html><body>"
                         "<h1>ESP32 LED Control</h1>"
-                        "<button onclick=\"toggleLED()\">Toggle LED</button>"
+                        "<p id=\"ledState\">LED is OFF</p>" // Default text
+                        "<button onclick=\"toggleLED(); updateLEDState();\">Toggle LED</button>"
                         "<script>"
                         "function toggleLED() {"
                         "  var xhr = new XMLHttpRequest();"
                         "  xhr.open('GET', '/toggle', true);"
                         "  xhr.send();"
                         "}"
+                        "function updateLEDState() {"
+                        "  var xhr = new XMLHttpRequest();"
+                        "  xhr.onreadystatechange = function() {"
+                        "    if (xhr.readyState == 4 && xhr.status == 200) {"
+                        "      document.getElementById('ledState').innerHTML = xhr.responseText;"
+                        "    }"
+                        "  };"
+                        "  xhr.open('GET', '/led-state', true);"
+                        "  xhr.send();"
+                        "}"
                         "</script>"
                         "</body></html>";
+
 
 /* An HTTP GET handler for the root page */
 esp_err_t get_handler(httpd_req_t *req) {
@@ -40,10 +54,33 @@ esp_err_t get_handler(httpd_req_t *req) {
 
 /* An HTTP GET handler to toggle the LED */
 esp_err_t toggle_handler(httpd_req_t *req) {
-    static bool led_state = false;
+    // static bool led_state = false;
     led_state = !led_state;
     gpio_set_level(LED_PIN, led_state);
     httpd_resp_send(req, NULL, 0); // Send an empty response to acknowledge
+    return ESP_OK;
+}
+
+esp_err_t get_led_state_handler(httpd_req_t *req) {
+    // char* resp_str = (gpio_get_level(LED_PIN) == 1) ? "LED is ON" : "LED is OFF";
+    // printf("%s", " --- MAIN --- LED state");
+    // printf("%d", gpio_get_level(LED_PIN));
+    // char* resp_str;
+    // if (gpio_get_level(LED_PIN) == 1){
+    //     resp_str = "LED is ON";
+    // }
+    // else{
+    //     resp_str = "LED is OFF";
+    // }
+    // httpd_resp_send(req, resp_str, HTTPD_RESP_USE_STRLEN);
+    char* resp_str;
+    if (led_state){
+        resp_str = "LED is ON";
+    }
+    else{
+        resp_str = "LED is OFF";
+    }
+    httpd_resp_send(req, resp_str, HTTPD_RESP_USE_STRLEN);
     return ESP_OK;
 }
 
@@ -62,6 +99,13 @@ httpd_uri_t uri_toggle = {
     .user_ctx  = NULL
 };
 
+httpd_uri_t uri_get_led_state = {
+    .uri       = "/led-state",
+    .method    = HTTP_GET,
+    .handler   = get_led_state_handler,
+    .user_ctx  = NULL
+};
+
 // start web server
 httpd_handle_t start_webserver(void) {
     httpd_config_t config = HTTPD_DEFAULT_CONFIG();
@@ -70,8 +114,26 @@ httpd_handle_t start_webserver(void) {
     if (httpd_start(&server, &config) == ESP_OK) {
         httpd_register_uri_handler(server, &uri_get);
         httpd_register_uri_handler(server, &uri_toggle);
+        httpd_register_uri_handler(server, &uri_get_led_state);
     }
     return server;
+}
+
+// ADDITIONAL FUNCTIONS
+char* load_file(char * filepath){
+    // load file to buffer
+    ESP_LOGI(TAG_webui, "Start to read file");
+    FILE *file = fopen(filepath, "r");
+    if (file == NULL){
+        ESP_LOGE(TAG_webui, "Cant open file");
+    }
+    int MAX_LEN = 5000;
+    char f_buffer[MAX_LEN];
+    while (fgets(f_buffer, MAX_LEN, file)){
+        printf("%s", f_buffer);
+    }
+
+    return f_buffer;
 }
 
 // void run_webui(void *pvParameters)
@@ -96,6 +158,6 @@ void run_webui()
     // Start the webserver
     httpd_handle_t server = start_webserver();
     if (server == NULL) {
-        ESP_LOGI(TAG, "Failed to start web server");
+        ESP_LOGI(TAG_webui, "Failed to start web server");
     }
 }
